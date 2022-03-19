622d4c8c83ecd6709c5a6315411af9a3d447e30c
aryan1403 <aaryan14032006@email.com>
2022-03-19 11:00:34 +0530

    update

--- Core Java/conditionalStatement.java
+++ Core Java/conditionalStatement.java
@@ -1,12 +1,14 @@
 public class conditionalStatement {
     public static void main(String[] args) {
         int a = 5;
-        int b = 7;
+        int b = 5;
 
         if(a > b) {
             System.out.println("a is greater");
-        } else {
+        } else if(a < b) {
             System.out.println("b is greater");
+        } else {
+            System.out.println("both are equal");
         }
     }
 }
