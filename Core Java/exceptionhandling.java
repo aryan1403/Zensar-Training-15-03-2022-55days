3e8af96869dfe22f360243aea855c352beee86fe
aryan1403 <aaryan14032006@email.com>
2022-03-21 14:34:08 +0530

    updated

--- Core Java/exceptionhandling.java
+++ Core Java/exceptionhandling.java
@@ -1,7 +1,11 @@
 public class exceptionhandling {
-    public static void main(String[] args) throws MyException {
-      
+    public static void main(String[] args) {
+        // Handling the exceptions
+        try {
             int result = 2 / 0;
+        } catch(Exception e) {
+            System.out.println("Exception Occured!");
+        }
             // throw new Exception("Cannot divide by zero");
             // System.out.println(result);
         
