28016b87530223a90f1120315a93649d21c03512
aryan1403 <aaryan14032006@email.com>
2022-03-21 14:11:19 +0530

    interfaces explained

---
+++ Core Java/impl.java
@@ -0,0 +1,18 @@
+public class impl implements interfacex {
+
+    @Override
+    public void walk() {
+        System.out.println("Walking...");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Running...");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Eating...");
+    }
+    
+}
---
+++ Core Java/interfacex.java
@@ -0,0 +1,5 @@
+public interface interfacex {
+    public void walk();
+    public void run();
+    public void eat();
+}
