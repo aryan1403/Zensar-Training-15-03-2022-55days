24c0c94d867b379ae264b18d1a05240c4c0abe63
aryan1403 <aaryan14032006@email.com>
2022-03-22 09:51:49 +0530

    strings explained

---
+++ day1/stringex.java
@@ -0,0 +1,30 @@
+public class stringex {
+    public static void main(String[] args) {
+        String s = "Aaryan is a cat";
+            //      012345
+
+        System.out.println(s.length()); // 6
+        System.out.println(s.charAt(3)); // y
+        System.out.println(s.substring(2, s.length())); // ryan
+        // beginIndex - Including, lastIndex - Excluded
+        // System.out.println(s.split(" ").toString()); arrays
+        System.out.println(s.equals("Aaryan is a cat"));
+        System.out.println(s.concat(" and human"));
+        System.out.println(s.indexOf("a")); // first occurence
+        System.out.println(s.lastIndexOf("a")); // last occurence
+        System.out.println(s.contains("dog")); 
+        System.out.println(s.startsWith("Aa"));
+        System.out.println(s.endsWith("cat"));
+        System.out.println(s.equalsIgnoreCase("aaryan is a cat"));
+
+        System.out.println(s.replace("cat", "human"));
+
+        int counter = 0; // record of how many times it occurs
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == 'c') { // if you find a increment the counter
+                counter++;
+            }
+        }
+        System.out.println("a occured = " + counter + " times");
+    }
+}
