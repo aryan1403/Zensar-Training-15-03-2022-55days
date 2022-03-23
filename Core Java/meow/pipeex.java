9ba94a00fee8aa2e7b6edec2692a50fe59aac813
aryan1403 <aaryan14032006@email.com>
2022-03-23 11:09:03 +0530

    updated

--- Core Java/meow/pipeex.java
+++ Core Java/meow/pipeex.java
@@ -5,17 +5,15 @@ import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 
 public class pipeex {
-
     public static void main(String[] args) {
         try {
             PipedInputStream pi = new PipedInputStream(); // input stream
-            PipedOutputStream po = new PipedOutputStream(pi);
+            PipedOutputStream po = new PipedOutputStream();
 
-            // pi.connect(po);
+            pi.connect(po);
             po.write(65); // ASCII value of A
+            // System.out.println(pi.available());
             System.out.println((char) pi.read());
-
-            // pi.available();
 
             pi.close();
             po.close();
---
+++ Core Java/meow/utftest.java
@@ -0,0 +1,19 @@
+package meow;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class utftest {
+    public static void main(String[] args) {
+        try {
+            RandomAccessFile raf = new RandomAccessFile("meow.txt", "rw");
+            // raf.seek(1);
+            raf.writeBytes("12345");
+            raf.close();
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
