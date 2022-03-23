6c484eb55887781a3730bb217ae11096bc7a8abb
aryan1403 <aaryan14032006@email.com>
2022-03-23 16:03:52 +0530

    handling primitive data types done

---
+++ Core Java/meow/hpd.java
@@ -0,0 +1,13 @@
+package meow;
+
+public class hpd {
+    public static void main(String[] args) {
+        String s = "100";
+
+        int a = Integer.parseInt(s);
+        System.out.println(a + 10);
+        char ch = ' ';
+        
+        System.out.println(Character.isSpaceChar(ch));
+    }
+}
