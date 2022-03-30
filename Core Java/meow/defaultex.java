9d01a8395685b641ebc3058dbd2af172d948ebae
aryan1403 <aaryan14032006@email.com>
2022-03-30 14:36:42 +0530

    intpredicate, doublefunction done

--- Core Java/meow/defaultex.java
+++ Core Java/meow/defaultex.java
@@ -1,5 +1,8 @@
 package meow;
 
+import java.util.function.DoubleFunction;
+import java.util.function.IntPredicate;
+
 public interface defaultex {
     public default void meow() {
         System.out.println("Meow Meow");
@@ -9,12 +12,32 @@ public interface defaultex {
     }
 }
 class mid implements defaultex {
+    // convert int -> boolean
+    IntPredicate predicate = (value) -> {
+        // Logic
+        if(value % 2 == 0) {
+            return true;
+        }
+        return false;
+    };
+
+    // func for double -> Generic
+    static DoubleFunction<cat> df = new DoubleFunction<>() {
+
+        @Override
+        public cat apply(double value) {
+            if(value == 0) {
+                return null;
+            }
+            return new cat();
+        } 
+    };
+
+    public static void main(String[] args) {
+        cat c = df.apply(10);
+        System.out.println(c.ex);
+    }
 }
 class cat {
     static defaultex ex = new mid();
-
-    public static void main(String[] args) {
-        ex.meow();
-        defaultex.m();
-    }
 }
