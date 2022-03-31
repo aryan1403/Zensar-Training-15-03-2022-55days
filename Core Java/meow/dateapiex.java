79c6e2a4a70cede3263cde04f8c63e94f955dfe0
aryan1403 <aaryan14032006@email.com>
2022-03-31 12:05:09 +0530

    meow meow

--- Core Java/meow/dateapiex.java
+++ Core Java/meow/dateapiex.java
@@ -1,10 +1,13 @@
 package meow;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.util.TimeZone;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class dateapiex {
     public static void main(String[] args) {
@@ -16,9 +19,26 @@ public class dateapiex {
         for (String s : tzones) {
             // System.out.println(s);
         }
+        System.out.println(Duration.ofSeconds(10));
+        Duration.ofSeconds(1);
+        // Period // Longer
+        // Timer
+        Timer t = new Timer();
+
+        System.out.println(LocalDateTime.now(ZoneId.of("Hongkong"))); 
+        t.schedule(new TimerTask() {
+    
+            @Override
+            public void run() {
+                System.out.println("Hello Aaryan");
+                
+            }
+            
+        }, 5000);
+        
+        System.out.println(t.toString());
         
         // LocalDateTime ldt = LocalDateTime.now(ZoneId.of("Asia/Tokyo"));
 
-        System.out.println(LocalDateTime.now(ZoneId.of("Hongkong")));   
     }
 }
